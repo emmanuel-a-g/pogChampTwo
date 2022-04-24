01d7e6e275034a50ed2fcb36f551f3f3d380ca01
Emmanuel Garcia <earturoga@gmail.com>
2022-04-24 00:36:18 -0600

    changing home page and css

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -1,115 +1,113 @@
-import React from 'react';
-import Carousel from 'react-material-ui-carousel';
-import { Grid, Paper } from '@material-ui/core';
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import { Grid, Paper } from "@material-ui/core";
 
 const description = [
   {
     name: "Expwnent: Up Your Game",
     description: "Battle in your favorite games to determine the champion!",
-    url: "https://nexus.leagueoflegends.com/wp-content/uploads/2019/06/Banner_T2_Image_tnp3w61gzna8r2n3rojp.jpg"
+    url: "https://nexus.leagueoflegends.com/wp-content/uploads/2019/06/Banner_T2_Image_tnp3w61gzna8r2n3rojp.jpg",
   },
   {
     name: "Organize Your Own Tournament",
     description: "Choose between Bracket or Swiss tournament styles",
-    url: "https://blog.playstation.com/tachyon/2019/11/ow2-featured.jpg?resize=1088,612&crop_strategy=smart&zoom=1"
+    url: "https://blog.playstation.com/tachyon/2019/11/ow2-featured.jpg?resize=1088,612&crop_strategy=smart&zoom=1",
   },
   {
     name: "Unleash the Winner in You",
     description: "Do you have what it takes to reach the final match?",
-    url: "https://cdn.gamer-network.net/2019/usgamer/Smash-Ultimate-Header-10.jpg/EG11/thumbnail/1920x1080/format/jpg/quality/65/super-smash-bros-ultimate-review-12072018.jpg"
-  }
-]
+    url: "https://cdn.gamer-network.net/2019/usgamer/Smash-Ultimate-Header-10.jpg/EG11/thumbnail/1920x1080/format/jpg/quality/65/super-smash-bros-ultimate-review-12072018.jpg",
+  },
+];
 
 const tournaments = [
   {
     name: "Scrabble",
-    location: "Buck's Game Shop"
+    location: "Buck's Game Shop",
   },
   {
     name: "LoL - Winter Split",
-    location: "Card Den"
+    location: "Card Den",
   },
   {
     name: "Smash Championship",
-    location: "Josh's Basement"
+    location: "Josh's Basement",
   },
   {
     name: "Magic the Gathering",
-    location: "Zilker Park"
+    location: "Zilker Park",
   },
   {
     name: "Hearthstone",
-    location: "Buck's Game Shop"
+    location: "Buck's Game Shop",
   },
   {
     name: "Poker",
-    location: "Card Den"
+    location: "Card Den",
   },
   {
     name: "Resident Evil",
-    location: "Josh's Basement"
+    location: "Josh's Basement",
   },
   {
     name: "Among Us",
-    location: "Discord"
-  }
-]
+    location: "Discord",
+  },
+];
 
-const MainPage = (props) => {
+const MainPage = () => {
   return (
-    <div>
-      <Grid container spacing={1} className="Description">
-        <Grid item xs={12} >
-          <Carousel animation="slide" interval={6000}>
-            {
-              description.map((item, i) => <DescriptionItem key={i} item={item} />)
-            }
-          </Carousel>
-        </Grid>
-      </Grid>
-      <Grid container spacing={0} className="tournaments">
-        <Grid item xs={12} className="tournamentCarousel">
-          <Carousel animation="slide" interval={10000}>
-            <Grid container spacing={1}>
-              {
-                tournaments.map((item, i) => <TournamentItem key={i} item={item} />)
-              }
-            </Grid>
-          </Carousel>
-        </Grid>
-      </Grid>
-
+    <div id="carouselHomeRec">
+      <div
+      id="carouselHomeDiv"
+      >
+        <Carousel animation="slide" interval={6000}
+        >
+          {description.map((item, i) => (
+            <DescriptionItem key={i} item={item} />
+          ))}
+        </Carousel>
+      </div>
+      <div id="homeBottom">
+        <Carousel animation="slide" interval={10000}>
+          {tournaments.map((item, i) => (
+            <TournamentItem key={i} item={item} />
+          ))}
+        </Carousel>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 function DescriptionItem(props) {
   return (
-    <Grid container spacing={0} className="carouselItem" style={{ "backgroundImage": props.item.url }}>
-      <Paper>
-        <Grid item xs={12} id='carouselText'>
-          <div className="carousel-inner-text">
-            <h4>{props.item.name}</h4>
-            <p>{props.item.description}</p>
-          </div>
-        </Grid>
-        <img className="carouselImg" src={props.item.url} />
-      </Paper>
-    </Grid>
-  )
+    <div
+      id="carouselItem"
+      style={{
+        backgroundImage: props.item.url,
+      }}
+    >
+      <div id="carouselText">
+        <h4>{props.item.name}</h4>
+        <p>{props.item.description}</p>
+      </div>
+      <img
+        className="carouselImg"
+        src={props.item.url}
+      />
+    </div>
+  );
 }
 
 function TournamentItem(props) {
   return (
-    <Grid item xs={3}>
-      <Paper>
-        <div className="TournamentPaper">
-          <h5>{props.item.name}</h5>
-          <p>{props.item.location}</p>
-        </div>
-      </Paper>
-    </Grid>
-  )
+    <div style={{ backgroundColor: "inherit" }}>
+      <div style={{ display: "flex" }}>
+        <h5>{props.item.name}</h5>
+        <p>{props.item.location}</p>
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import axios from 'axios';
-
 const SideBar = (props) => {
-
-
 
   return (
     <div id='sideBar'>
