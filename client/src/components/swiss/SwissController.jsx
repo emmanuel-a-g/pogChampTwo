a2e45b42eca1f6f9ac89f5e0c38d6341cd0c5f5d
Emmanuel Garcia <earturoga@gmail.com>
2022-06-15 19:19:11 -0600

    fixed swiss winners

--- client/src/components/swiss/SwissController.jsx
+++ client/src/components/swiss/SwissController.jsx
@@ -164,7 +164,6 @@ const SwissController = (props) => {
     }
 
     setPairs(newPairs);
-
     if (gameDetails.currentRound <= Number(gameDetails.rounds)) {
       gameDetails.currentRound++;
     }
@@ -238,7 +237,7 @@ const SwissController = (props) => {
   // <Button onClick={resetPage} variant="contained" color="primary">Reset?</Button>
 
   return (
-    <div maxWidth="lg" className="swissPairing">
+    <div className="swissPairing">
       <h2>Swiss Tournament</h2>
       <div className="game-details">
         {gameDetails.tournamentName ? (
@@ -308,7 +307,7 @@ const SwissController = (props) => {
           <p>At least 2 players to start: </p>
           {gameDetails.winner.length >= 1 && (
             <Button
-              variant="filled"
+              variant="contained"
               type="submit"
               onClick={() => window.print()}
             >
@@ -346,16 +345,16 @@ const SwissController = (props) => {
           {gameDetails.winner !== "" && !Array.isArray(gameDetails.winner) ? (
             <Container maxWidth="sm" className="pairings-container">
               <h2>
-                {gameDetails.winner} wins ${money.first}!
+                {gameDetails.winner} wins #1 place {money.first}!
               </h2>
               {winners.second && (
                 <h2>
-                  {winners.second} ${money.second}!
+                  {winners.second} wins #2 place {money.second}!
                 </h2>
               )}
               {winners.third && (
                 <h2>
-                  {winners.third} ${money.third}!
+                  {winners.third} {money.third}!
                 </h2>
               )}
             </Container>
