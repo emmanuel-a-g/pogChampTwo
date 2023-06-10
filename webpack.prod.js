31c2db2dc4c5d8d3d0ece77303f85f8b88d92739
Emmanuel Garcia <earturoga@gmail.com>
2023-06-09 22:47:51 -0600

    Some changes to webpack and package json

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: path.join(__dirname, "client", "src", "index.js"),
   output: {
     path: path.join(__dirname, "client", "dist"),
-    filename: "bundle.js"
+    filename: "./bundle.js",
   },
   module: {
     rules: [
@@ -14,29 +14,24 @@ module.exports = {
         test: /.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
+        use: ["file-loader"],
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       favicon: path.join(__dirname, "client", "public", "favi.ico"),
       filename: "index.html",
-      template: path.join(__dirname, "client", "public", "index.html")
-    })
-  ]
+      template: path.join(__dirname, "client", "public", "index.html"),
+    }),
+  ],
 };
